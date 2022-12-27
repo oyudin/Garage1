a14f1f3161bf4eba74115835bf5f83a72b719dd8
saniayudin <olex.yudin@gmail.com>
2022-12-27 13:33:33 +0200

    init commit

---
+++ src/main/java/com/example/garage1/Garage1Application.java
@@ -0,0 +1,13 @@
+package com.example.garage1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Garage1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Garage1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/garage1/Garage1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.garage1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Garage1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
